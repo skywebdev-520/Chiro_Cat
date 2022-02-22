8ebe1be7fa8b307e92c6c67b63beea186157a071
sky <sky@gmail.com>
2022-02-22 12:59:40 +0300

    change AuthFacebookController

--- platform/plugins/real-estate/src/Http/Controllers/AuthFacebookController.php
+++ platform/plugins/real-estate/src/Http/Controllers/AuthFacebookController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Socialite;
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Hash;
 
 class AuthFacebookController extends Controller
 {
